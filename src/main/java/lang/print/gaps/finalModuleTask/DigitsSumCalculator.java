8ab6cdf8a77ff782b4eb852c44b501a5045b5fc7
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-17 19:24:16 +0500

    s

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int a = number%10;
+        int b = (number-a)/10;
+        int c = b%10;
+        int d = (b-c)/10;
+        int l = d%10;
+        int k  = d-l;
+        System.out.println(a+b+l+k);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int a = number%10;
+        int b = (number-a)/10;
+        int c = b%10;
+        int k =(b-c)/10;
+        int l =a*100+c*10+k;
+        System.out.println(l);
     }
 }
